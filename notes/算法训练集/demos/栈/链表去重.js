8bf3035496c06e955c3e04d4797dae99573ddc2b
wagnxx <2362999724@qq.com>
2020-05-12 23:01:01 +0800

    link-list remove same node

---
+++ notes/算法训练集/demos/栈/链表去重.js
@@ -0,0 +1,70 @@
+/**
+
+83. 删除排序链表中的重复元素
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+
+
+*/
+
+
+/**
+ 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var deleteDuplicates = function(head) {
+    
+    if(head == null ) return head;
+
+    // 递归
+    // head.next = deleteDuplicates(head.next);
+
+    // if(head.val == head.next.val) head = head.next;
+
+    let current = head;
+    while (current != null && current.next != null) {
+        if (current.next.val == current.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+
+
+ 
+
+ 
+}
+ 
+
+
+/**
+
+方法：直接法
+算法
+
+这是一个简单的问题，仅测试你操作列表的结点指针的能力。由于输入的列表已排序，因此我们可以通过将结点的值与它之后的结点进行比较来确定它是否为重复结点。如果它是重复的，我们更改当前结点的 next 指针，以便它跳过下一个结点并直接指向下一个结点之后的结点。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/solution/shan-chu-pai-xu-lian-biao-zhong-de-zhong-fu-yuan-s/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+*/
