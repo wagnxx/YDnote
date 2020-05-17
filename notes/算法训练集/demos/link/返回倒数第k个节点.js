32e0fdb032908ec00c54cd6faf8a966a0e6d4f9b
wagnxx <2362999724@qq.com>
2020-05-17 21:54:32 +0800

    algorithm demo practice

---
+++ notes/算法训练集/demos/link/返回倒数第k个节点.js
@@ -0,0 +1,45 @@
+/**
+
+实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
+
+注意：本题相对原题稍作改动
+
+示例：
+
+输入： 1->2->3->4->5 和 k = 2
+输出： 4
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {number}
+ */
+ var kthToLast = function(head, k) {
+
+    let arr=[];
+    let cur = head;
+    while(cur){
+        let next = cur.next;
+        arr.push(cur);
+        cur = next;
+    }
+
+    return arr[arr.length-k].val;
+};
